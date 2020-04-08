aa493f7b5ddec4b42bf6d0c1e752396d72edff54
Alberto Mardegan <mardy@users.sourceforge.net>
2020-04-09 00:22:41 +0300

    qtbase: use dynamic configuration of OpenGL backend
    This turns on the fallback behavior introduced in Qt 5.4 and described
    in
    
    https://www.qt.io/blog/2014/11/27/qt-weekly-21-dynamic-opengl-implementation-loading-in-qt-5-4
    
    This is how Qt builds its official packages.

--- src/qtbase.mk
+++ src/qtbase.mk
@@ -9,7 +9,7 @@ $(PKG)_CHECKSUM := 48b9e79220941665a9dd827548c6428f7aa3052ccba8f4f7e039a94aa1d2b
 $(PKG)_SUBDIR   := $(PKG)-everywhere-src-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-everywhere-src-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://download.qt.io/official_releases/qt/5.14/$($(PKG)_VERSION)/submodules/$($(PKG)_FILE)
-$(PKG)_DEPS     := cc dbus fontconfig freetds freetype harfbuzz jpeg libmysqlclient libpng openssl pcre2 postgresql sqlite zlib zstd
+$(PKG)_DEPS     := cc dbus fontconfig freetds freetype harfbuzz jpeg libmysqlclient libpng openssl pcre2 postgresql sqlite zlib zstd mesa
 $(PKG)_DEPS_$(BUILD) :=
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 
@@ -44,7 +44,7 @@ define $(PKG)_BUILD
             $(if $(BUILD_STATIC), -static,)$(if $(BUILD_SHARED), -shared,) \
             -prefix '$(PREFIX)/$(TARGET)/qt5' \
             -no-icu \
-            -opengl desktop \
+            -opengl dynamic \
             -no-glib \
             -accessibility \
             -nomake examples \
